9aeaa15a0c01c0f989a3971ba53eb4abb4f17e91
Amitdaskne <amitdaskishanganj@gmail.com>
2026-06-20 19:29:03 +0530

    Add feed reels chat profile screens

---
+++ app/src/main/java/com/amitgram/app/ReelsActivity.kt
@@ -0,0 +1,11 @@
+package com.amitgram.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ReelsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reels)
+    }
+}
